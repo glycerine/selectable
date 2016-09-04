6052cc9baf8959ed3c3c033820d27648cfbc7659
Jason E. Aten <j.e.aten@gmail.com>
2016-09-04 13:00:04 -0700

    atg. reuse of barrier after ReleaseAndReset tested. Test shutdown.

--- barrier_test.go
+++ barrier_test.go
@@ -9,6 +9,41 @@ import (
 func Test001BarrierReleasesAllGoroutines(t *testing.T) {
 	cv.Convey("selectable.Barrier should release all waiting goroutines when ReleaseAndReset is sent a struct{}{}", t, func() {
 
+		b := NewBarrier()
+		m := 5 // number of re-uses of the barrier to conduct.
+
+		for j := 0; j < m; j++ {
+			// use the WaitGroup sync primitive to verify our Barrier
+			n := 3
+			wgUp := &sync.WaitGroup{}
+			wgUp.Add(n)
+
+			wgDown := &sync.WaitGroup{}
+			wgDown.Add(n)
+
+			for i := 0; i < n; i++ {
+				go func() {
+					ch := b.Wait() // normally we could receive immediately here;
+					wgUp.Done()    // but we want to coordinate with wgUp too here.
+					select {
+					case <-ch: // normally <- b.Wait() is fine.
+						wgDown.Done()
+					case <-b.Done:
+						// good form to always have this in our selects.
+					}
+				}()
+			}
+			wgUp.Wait()
+			b.ReleaseAndReset <- struct{}{}
+			wgDown.Wait()
+			cv.So(true, cv.ShouldBeTrue) // we should get to here.
+		}
+	})
+}
+
+func Test002BarrierShutdownDoesntDeadlock(t *testing.T) {
+	cv.Convey("selectable.Barrier on shutdown should not deadlock", t, func() {
+
 		// use the WaitGroup sync primitive to verify our Barrier
 		n := 3
 		wgUp := &sync.WaitGroup{}
@@ -24,14 +59,13 @@ func Test001BarrierReleasesAllGoroutines(t *testing.T) {
 				wgUp.Done()    // but we want to coordinate with wgUp too here.
 				select {
 				case <-ch: // normally <- b.Wait() is fine.
-					wgDown.Done()
 				case <-b.Done:
-					// good form to always have this in our selects.
+					wgDown.Done()
 				}
 			}()
 		}
 		wgUp.Wait()
-		b.ReleaseAndReset <- struct{}{}
+		close(b.RequestStop)
 		wgDown.Wait()
 		cv.So(true, cv.ShouldBeTrue) // we should get to here.
 	})
